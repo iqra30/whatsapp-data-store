ac4b193cf2ba5f0ed28f01e1dd43445ffe54aaf6
iqra30 <51120066+iqra30@users.noreply.github.com>
2019-07-11 15:43:08 +0500

    Add files via upload

---
+++ page.js
@@ -0,0 +1,88 @@
+// Create a data store for whatsapp//
+
+let contact1 ={
+    name: "Alaina Sarfaraz",
+    number: "0321456987",
+    lastSeen: "2:34pm",
+    isRead: true,
+    date: "11 / 7 / 2019",
+    lastMessage: "hgehjtshvjf"
+}
+let contact2 = {
+    name: "Mahanoor Faraz",
+    number: "03214552435",
+    lastSeen: "7:30pm",
+    isRead: true,
+    date: "9 / 7 / 2019",
+    lastMessage: "hi..gehjtshvjf"
+}
+let contact3 = {
+    name: "Arbaz Khan",
+    number: "03214524327",
+    lastSeen: "5:34pm",
+    isRead: false,
+    date: "10 / 7 / 2019",
+    lastMessage: "hgehjtscfyhfchhvjf"
+}
+let contact4 = {
+    name: "Shahrukh Siddiqui",
+    number: "032145656254",
+    lastSeen: "1:27am",
+    isRead: true,
+    date: "11 / 7 / 2019",
+    lastMessage: "hghgjhfutdkjuykiyehjtshvjf"
+}
+let contact5 = {
+    name: "Saqib Siddiqui",
+    number: "03214525344",
+    lastSeen: "8:54pm",
+    isRead: true,
+    date: "1 / 7 / 2019",
+    lastMessage: "hyjfjfgehjtshvjf"
+}
+
+
+
+let contants =[
+    {
+        name: "Alaina Sarfaraz",
+        number: "0321456987",
+        lastSeen: "2:34pm",
+        isRead: true,
+        date: "11 / 7 / 2019",
+        lastMessage: "hgehjtshvjf"
+    },
+    {
+        name: "Mahanoor Faraz",
+        number: "03214552435",
+        lastSeen: "7:30pm",
+        isRead: true,
+        date: "9 / 7 / 2019",
+        lastMessage: "hi..gehjtshvjf"
+    },
+    {
+        name: "Arbaz Khan",
+        number: "03214524327",
+        lastSeen: "5:34pm",
+        isRead: false,
+        date: "10 / 7 / 2019",
+        lastMessage: "hgehjtscfyhfchhvjf"
+    },
+    {
+        name: "Shahrukh Siddiqui",
+        number: "032145656254",
+        lastSeen: "1:27am",
+        isRead: true,
+        date: "11 / 7 / 2019",
+        lastMessage: "hghgjhfutdkjuykiyehjtshvjf"
+    },
+    {
+        name: "Saqib Siddiqui",
+        number: "03214525344",
+        lastSeen: "8:54pm",
+        isRead: true,
+        date: "1 / 7 / 2019",
+        lastMessage: "hyjfjfgehjtshvjf"
+    }
+
+]
